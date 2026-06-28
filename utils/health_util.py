5ce97cceb3b765878781dccb9e5dd3869cc9fb1b
magellan41 <magellan@stu.gxmzu.edu.cn>
2026-06-28 21:08:52 +0800

    修改了工具返回形式

--- utils/health_util.py
+++ utils/health_util.py
@@ -18,10 +18,17 @@ def select_last_day_health_data():
     steps_model_list = [StepModel(steps=step.steps, synced_time=step.synced_time) for step in steps]
     heart_rates = heart_rate_orm_obj.select_last_day()
     heart_rates_model_list = [HeartRateModel(avg_bpm=heart_rate.avg_bpm, max_bpm=heart_rate.max_bpm, min_bpm=heart_rate.min_bpm, latest_bpm=heart_rate.latest_bpm, latest_time=heart_rate.synced_time,sample_count = heart_rate.sample_count) for heart_rate in heart_rates]
-    res = LastDayHealthModel(
-        sleep_sessions=sleep_sessions_model_list,
-        steps=steps_model_list,
-        heart_rates=heart_rates_model_list
-    )
+    # res = LastDayHealthModel(
+    #     sleep_sessions=sleep_sessions_model_list,
+    #     steps=steps_model_list,
+    #     heart_rates=heart_rates_model_list
+    # )
+    sleep_data_str = "【睡眠数据】\n"
+    for sleep_session in sleep_sessions:
+        sleep_data_str += f"本轮睡眠开启时间:{sleep_session.start_time}, 本轮睡眠结束时间:{sleep_session.end_time}\n"
+        sleep_data_str += "\n\t".join([f"{stage.start_time}至{stage.end_time}处于{stage.stage}阶段" for stage in sleep_session.stages])
+    step_data_str = "【步数数据】\n" + "\n".join([f"在{step.synced_time}时步数为{step.steps}步" for step in steps])
+    heart_rate_data_str = "【心率数据】\n" + "\n".join([f"在{heart_rate.synced_time}时最大心率{heart_rate.max_bpm}pm, 最小心率{heart_rate.min_bpm}pm, 平均心率{heart_rate.avg_bpm}pm, 当时心率{heart_rate.latest_bpm}pm" for heart_rate in heart_rates])
 
-    return res.model_dump_json()
+    res = f"{sleep_data_str}\n{step_data_str}\n{heart_rate_data_str}"
+    return res
